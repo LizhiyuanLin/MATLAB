39805ff10e3dc43b9b08abd8f53d30086afb1f59
Lizhiyuan Lin <lizhiyuanlin@gmail.com>
2021-11-15 15:11:22 -0500

    fix bug

--- A3/composite_simpson.m
+++ A3/composite_simpson.m
@@ -8,7 +8,7 @@ function [C1, composite_simpson] = composite_simpson(f, a, b, n)
         f_t2i = f_t2i+f(t2i);
         C1 = C1+1;
     end
-    for i=1:n/2-1
+    for i=1:n/2
         t2i_minus_1 = a+(2*i-1)*h;
         f_t2i_minus_1 = f_t2i_minus_1+f(t2i_minus_1);
         C1 = C1+1;
