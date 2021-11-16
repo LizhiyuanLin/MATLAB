e3f6e5b1459407861b406d003c95ceab6b1d6f14
Lizhiyuan Lin <lizhiyuanlin@gmail.com>
2021-11-15 20:02:45 -0500

    use his algorithm

--- A3/adaptive_simpson.m
+++ A3/adaptive_simpson.m
@@ -1,24 +1,16 @@
-function [C2, simpson_result] = adaptive_simpson(f, a, b, epsilon, level, level_max)
-    C2 = 0;
-    level = level+1;
+function [C2, simpson_result] = adaptive_simpson(f, a, b, tol)
     h = b-a;
     c = (a+b)/2;
-    one_simpson = h*(f(a)+4*f(c)+f(b))/6;
-    C2 = C2+3;
-    d = (a+c)/2;
-    e = (c+b)/2;
-    two_simpson = h*(f(a)+4*f(d)+2*f(c)+4*f(e)+f(b))/12;
-    C2 = C2+5;
-    if level >= level_max
-        simpson_result = two_simpson;
+    S1 = h/6*(f(a)+4*f((a+b)/2)+f(b));
+    S2 = h/12*(f(a)+4*f((a+c)/2)+2*f(c)+4*f((c+b)/2)+f(b));
+    C2 = 8;
+    E2 = (S2-S1)/15;
+    if abs(E2)<=tol
+        simpson_result = S2+E2;
     else
-        if abs(two_simpson-one_simpson) < 15*epsilon
-            simpson_result = two_simpson+(two_simpson-one_simpson)/15;
-        else
-            [C2_left, left_simpson] = adaptive_simpson(f, a, c, epsilon/2, level, level_max);
-            [C2_right, right_simpson] = adaptive_simpson(f, c, b, epsilon/2, level, level_max);
-            simpson_result = left_simpson+right_simpson;
-            C2 = C2+C2_left+C2_right;
-        end
+        [C_left, Q1] = adaptive_simpson(f, a, c, tol/2);
+        [C_right, Q2] = adaptive_simpson(f, c, b, tol/2);
+        C2 = C_left+C_right;
+        simpson_result = Q1+Q2;
     end
 end
