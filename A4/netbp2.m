54601fb62bf34987bddc4a3a434e3b298b6747a3
Lizhiyuan Lin <lizhiyuanlin@gmail.com>
2021-11-25 00:33:14 -0500

    fix bug

--- A4/netbp2.m
+++ A4/netbp2.m
@@ -41,7 +41,7 @@ semilogy([1:1e4:niter],cost(1:1e4:niter))
   function costval = cost_function(W2,W3,W4,b2,b3,b4)
      costvec = zeros(num_of_points,1); 
      for i = 1:num_of_points
-         x = data(:,k);
+         x = data(:,i);
          a2 = activate(x,W2,b2);
          a3 = activate(a2,W3,b3);
          a4 = activate(a3,W4,b4);
