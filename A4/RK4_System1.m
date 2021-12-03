30d6cd556059a61ca50fbe5dea12939cb49045e1
Lizhiyuan Lin <lizhiyuanlin@gmail.com>
2021-12-03 10:12:45 -0500

    no need for an n here

--- A4/RK4_System1.m
+++ A4/RK4_System1.m
@@ -3,19 +3,19 @@ function x_output = RK4_System1(n,h,t,xi,nsteps)
     Kij = zeros(n,4);
     yi = zeros(n,1);
     for j=1:nsteps
-        Kij(:,1) = XP_System(n,t,xi);
+        Kij(:,1) = XP_System(t,xi);
         for i=1:n
         	yi(i) = xi(i)+0.5*h*Kij(i,1);
         end
-        Kij(:,2) = XP_System(n,t+h/2,yi);
+        Kij(:,2) = XP_System(t+h/2,yi);
         for i=1:n
         	yi(i) = xi(i)+0.5*h*Kij(i,2);
         end
-        Kij(:,3) = XP_System(n,t+h/2,yi);
+        Kij(:,3) = XP_System(t+h/2,yi);
         for i=1:n
         	yi(i) = xi(i)+h*Kij(i,3);
         end
-        Kij(:,4) = XP_System(n,t+h,yi);
+        Kij(:,4) = XP_System(t+h,yi);
         for i=1:n
         	xi(i) = xi(i)+h/6*(Kij(i,1)+2*Kij(i,2)+2*Kij(i,3)+Kij(i,4));
         end
--- A4/XP_System.m
+++ A4/XP_System.m
@@ -1,7 +1,7 @@
-function fi = XP_System(n,t,xi)
+function fi = XP_System(t,xi)
     miu = 0.012277471;
     miu_hat = 1-miu;
-    fi = zeros(n,1);
+    fi = zeros(4,1);
     fi(1) = xi(2);
     fi(2) = xi(1)+2*xi(4)-miu_hat*(xi(1)+miu)/((xi(1)+miu)^2+xi(3)^2)^(3/2)-miu*(xi(1)-miu_hat)/((xi(1)-miu_hat)^2+xi(3)^2)^(3/2);
     fi(3) = xi(4);
